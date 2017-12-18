99505944032edc6bc676533a9a326e4d17c2da46
Josh <jyi468@gmail.com>
2017-12-18 09:46:44 -0500

    Update and add Chart

--- wine-quality/stats.py
+++ wine-quality/stats.py
@@ -9,6 +9,65 @@ from sklearn.linear_model import Ridge
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 
+wine = pd.read_csv("data/winequality-red.csv", sep=';')
+
+
+def plot_pearson_r_horizontal(winedata, stat):
+    # Print pearson for each column
+    for i in range(11):
+        feature = winedata.iloc[:, i]
+        feature_name = feature.name
+        pearson_r = sp.stats.pearsonr(feature, target)
+
+        # add pearson's r information to dict
+        stat[feature_name]['pearson_r'] = pearson_r[0]
+        # add to pearson_list with absolute values to compare with feature importances
+        pearson_list.append(math.fabs(pearson_r[0]))
+        # print('', feature_name, 'r: ', pearson_r[0])
+
+    # y label are features, labels going in reverse order
+    # x should be pearson r value
+    y_pos = np.arange(11)
+
+    fig, ax = plt.subplots()
+
+    ax.barh(y_pos, pearson_list)
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(feature_names)
+    ax.set_xlabel('Pearson R (abs)')
+
+    plt.show()
+
+
+# Plot histograms for all features
+def plot_feature_histograms(winedata):
+    # Create subplot matrix of 3 rows and 4 columns
+    fig, axes = plt.subplots(3, 4)
+    axes = axes.ravel()
+    # Print pearson for each column
+    for i, ax in enumerate(axes):
+        feature = winedata.iloc[:, i]
+        feature_name = feature.name
+        # num_bins must be appropriate for data spread - use descriptive stats to determine?
+        if i == 11:
+            num_bins = 5
+        else:
+            num_bins = 40
+        ax.hist(feature, num_bins, alpha=0.5, histtype='bar', edgecolor='black')
+        ax.set_title(feature_name)
+        ax.set_ylabel('Count')
+
+    # plt.tight_layout(pad=0.25, rect=[0, 0, 1, 1])
+    fig.suptitle('Feature Counts', fontsize=16)
+    plt.subplots_adjust(top=0.9,
+                        bottom=0.042,
+                        left=0.037,
+                        right=0.99,
+                        hspace=0.241,
+                        wspace=0.205)
+    plt.show()
+
+
 # Plot feature importances
 def plot_feature_importances(model):
     n_features = data.shape[1]
@@ -19,15 +78,14 @@ def plot_feature_importances(model):
     plt.show()
 
 
-wine = pd.read_csv("data/winequality-red.csv", sep=';')
-
 # print(wine.shape)
 # print(wine.keys())
 # print(wine.iloc[:, 0:11].shape)
 # print(wine.iloc[:, 11])
 
 # TODO: Remove outliers
-# print(wine.describe())
+print(wine.describe())
+
 
 # Put data into correct format. X data will be all columns except quality. y data will be quality (target)
 data = wine.iloc[:, 0:11]
@@ -49,33 +107,13 @@ feature_names = wine.columns.values[:11]
 stats = {k: {} for k in feature_names}
 pearson_list = []
 
-# Print pearson for each column
-for i in range(11):
-    feature = wine.iloc[:, i]
-    feature_name = feature.name
-    pearson_r = sp.stats.pearsonr(feature, target)
+# Create histograms of data distributions for all features - show in one plot
+# Create box and whisker plots
+# Show data fitting before and after outliers
+# Show most important positive and negative features
 
-    # add pearson's r information to dict
-    stats[feature_name]['pearson_r'] = pearson_r[0]
-    # add to pearson_list with absolute values to compare with feature importances
-    pearson_list.append(math.fabs(pearson_r[0]))
-    # print('', feature_name, 'r: ', pearson_r[0])
-
-# print(stats)
-
-# TODO: create horizontal bar chart with pearson and compare to feature importances
-# y label are features, labels going in reverse order
-# x should be pearson r value
-y_pos = np.arange(11)
-
-fig, ax = plt.subplots()
-
-ax.barh(y_pos, pearson_list)
-ax.set_yticks(y_pos)
-ax.set_yticklabels(feature_names)
-ax.set_xlabel('Pearson R (abs)')
-
-plt.show()
+# plot_pearson_r_horizontal(wine, stats)
+plot_feature_histograms(wine)
 
 # pearson_alcohol = sp.stats.pearsonr(alcohol, target)
 # pearson_sulphates = sp.stats.pearsonr(sulphates, target)
